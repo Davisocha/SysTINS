8f1185293eb2f76d8cdb0f739349f4bf082e328c
davi araujo socha <daviaraujosocha@gmail.com>
2024-11-14 17:23:47 -0300

    em reforma

--- SysTINSApp/FrmNovoCliente.cs
+++ SysTINSApp/FrmNovoCliente.cs
@@ -23,11 +23,18 @@ namespace SysTINSApp
             int idUser = Convert.ToInt32(dgvClientes.Rows[linhaAtual].Cells[0].Value);//ele recupera o valor zero da linha id
             var cliente = Cliente.ObterClientePorID(idUser);
             ckbAtivo.Visible = true;
+            btnInserir.Enabled = false;
             txtId.Text = cliente.Id.ToString();
+            
             txtNome.Text = cliente.Nome;
+            
             txtCPF.Text = cliente.CPF;
+            txtCPF.Enabled = false;
+            
             txtTelefone.Text = cliente.Telefone;
+            
             txtEmail.Text = cliente.Email;
+            txtEmail.Enabled = false;
             dtpData_nasc.Value = (DateTime)cliente.Data_nasc;
             ckbAtivo.Checked = cliente.Ativo;
             btnAtualizar.Visible = true;
@@ -75,8 +82,8 @@ namespace SysTINSApp
             {
                 txtId.Text = cliente.Id.ToString();
                 CarregaGridDeClientes();
-
-                //MessageBox.Show($"Cliente Cadastrado com o ID {cliente.Id}");
+                MessageBox.Show($"Cliente Cadastrado com o ID {cliente.Id}");
+                btnInserir.Enabled = false;
 
             }
             else
@@ -84,24 +91,9 @@ namespace SysTINSApp
                 MessageBox.Show("Falha ao Inserir Cliente!");
             }
         }
-        public void mensagem()
-        {
-            FrmNovoCliente testDialog = new();
-            {
-                if (testDialog.ShowDialog(this) == DialogResult.OK)
-                {
-                    FrmAdicionarEndereco frmadicionarEndereco = new();
-                    frmadicionarEndereco.MdiParent = this;
-
-                    frmadicionarEndereco.Show();
-                }
-                testDialog.Dispose();
 
 
-            }
 
-
-        }
 
         private void button1_Click(object sender, EventArgs e)//adicionar endereço
         {
@@ -118,15 +110,19 @@ namespace SysTINSApp
             Cliente cliente = new();
             cliente.Id = int.Parse(txtId.Text); 
             cliente.Nome = txtNome.Text;
-            cliente. = TxtSenha.Text;
-            usuario.Nivel = Nivel.ObterPorId(Convert.ToInt32(CmbNivel.SelectedValue));//pega o metodo obter por id, vai pegar o id converter em int para o cmbnivel
-            if (usuario.Atualizar())// se o metodo atualizar der true, irá mostrar uma messagebox
+            cliente.Telefone = txtTelefone.Text;
+            cliente.Telefone = txtTelefone.Text;
+            cliente.Data_nasc = dtpData_nasc.Value;
+            if (cliente.Atualizar())// se o metodo atualizar der true, irá mostrar uma messagebox
             {
-                CarregaGridUsuarios();
-                MessageBox.Show("Usuário atualizado com sucesso!");
+
+                CarregaGridDeClientes();
+                MessageBox.Show("Cliente atualizado com sucesso!");
             }
         }
     }
 
 }
 
+
+        
